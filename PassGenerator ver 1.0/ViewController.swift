12f0d06317ec8f9fb36a9f204319c3ff19a3e867
ahmed seddek <ahmedseddek@learniosfreecoms-mac.local>
2017-01-07 13:19:34 +0200

    Initial Commit

---
+++ PassGenerator ver 1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PassGenerator ver 1.0
+//
+//  Created by ahmed seddek on 1/7/17.
+//  Copyright © 2017 epicMoments. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PassGenerator ver 1.0Tests/PassGenerator_ver_1_0Tests.swift
@@ -0,0 +1,36 @@
+//
+//  PassGenerator_ver_1_0Tests.swift
+//  PassGenerator ver 1.0Tests
+//
+//  Created by ahmed seddek on 1/7/17.
+//  Copyright © 2017 epicMoments. All rights reserved.
+//
+
+import XCTest
+@testable import PassGenerator_ver_1_0
+
+class PassGenerator_ver_1_0Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
